78bd77e6407fc62ce3bfacf132c8ecae80e6051d
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2025-01-07 10:23:32 +0100

    add dynamic collections page

--- src/app/portfolio/collections/[collection]/page.tsx
+++ src/app/portfolio/collections/[collection]/page.tsx
@@ -1,10 +1,10 @@
 import Collection from "@/app/ui/collection";
 
 const Page = async ({ params }: { params: Promise<{ collection: string }> }) => {
-  const collectionID = (await params).collection
+  const collection = (await params).collection
 
   return(
-    <Collection collectionID={collectionID} />
+    <Collection collection={collection} />
   )
 }
 
