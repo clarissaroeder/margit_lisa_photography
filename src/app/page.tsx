a38fb710caa923b6dc71bb0e50f29ad4ffe32739
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2025-01-07 19:01:03 +0100

    remove unnecessary div wrapping slideshow component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Slideshow from "./ui/slideshow";
 
 export default function Home() {
   return (
-    <div className="flex items-center h-full w-full">
-      <Slideshow />
-    </div>
+    <Slideshow />
   );
 }
